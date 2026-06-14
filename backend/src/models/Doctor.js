1241ff9050c2609851f1b2a62793e07cf3d10576
tomchienxuu <truclingg194@gmail.com>
2026-06-14 11:28:42 +0700

    cap nhat giao dien bac si

--- backend/src/models/Doctor.js
+++ backend/src/models/Doctor.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const doctorSchema = new mongoose.Schema({
-    doctorID: { type: String, required: false, unique: true },
     qualification: { type: String, required: false },
     academicTitle: { type: String, required: false },
     academicDegree: { type: String, required: false },
